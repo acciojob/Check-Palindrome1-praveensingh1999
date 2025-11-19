f91b7fe525ac94744f65d0db847b845c89614273
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-11-19 20:52:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 // complete the given function
 
 function palindrome(str){
-str.trim();
-	let first =0;
-	let last = str.length - 1;
-	while(first<last){
-		if(str[first]/toLowerCase()!=str[last].toLowerCase()){
-			return false;
-		}
-	}
-	return true;
+ let cleaned = "";
+  for (let char of s.toLowerCase()) {
+    if (
+      (char >= 'a' && char <= 'z') ||
+      (char >= '0' && char <= '9')
+    ) {
+      cleaned += char;
+    }
+  }
+
+  // Two-pointer check
+  let left = 0;
+  let right = cleaned.length - 1;
+
+  while (left < right) {
+    if (cleaned[left] !== cleaned[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
 }
+
 module.exports = palindrome
