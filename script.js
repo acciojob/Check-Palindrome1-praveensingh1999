d0d22fbfe53f34ef455f87ad9c44ca7427c538b1
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-11-19 20:46:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
-
+str.trim();
+	let first =0;
+	let last = str.length - 1;
+	while(first<last){
+		if(str[first]/toLowerCase()!=str[last].toLowerCase()){
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
